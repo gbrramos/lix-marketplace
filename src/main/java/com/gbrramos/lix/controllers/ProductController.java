9674b882c85f140d7c68c306187475117295138f
Gabriel Ramos <gabriel.olv2002@gmail.com>
2022-11-28 19:56:23 +0000

    finish products crud

--- src/main/java/com/gbrramos/lix/controllers/ProductController.java
+++ src/main/java/com/gbrramos/lix/controllers/ProductController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gbrramos.lix.models.JsonResponse;
 import com.gbrramos.lix.models.Product;
 import com.gbrramos.lix.repositories.IProductRepository;
-
 
 @RestController
 @RequestMapping("/api/products")
@@ -40,7 +40,7 @@ public class ProductController {
         if(!product.isPresent()) 
             return new ResponseEntity<JsonResponse> (new JsonResponse("Product not found", 404, product), null, 404);
 
-        return new ResponseEntity<JsonResponse>(new JsonResponse("Ok", 200, product), null, 200);
+        return new ResponseEntity<JsonResponse>(new JsonResponse("ok", 200, product), null, 200);
     }
 
     @PostMapping
@@ -54,13 +54,24 @@ public class ProductController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Product> update (@PathVariable long id, @RequestBody Product rProduct) {
+    public ResponseEntity<JsonResponse> update (@PathVariable long id, @RequestBody Product rProduct) {
         try {
-            
+            rProduct.setId(id);
+            productRepository.save(rProduct);
         } catch (Exception e) {
-            // TODO: handle exception
+            return new ResponseEntity<JsonResponse>(new JsonResponse(e.getMessage(), 500, null), null, 500);
         }
-        return new ResponseEntity<Product>(rProduct, null, 200);
+        return new ResponseEntity<JsonResponse>(new JsonResponse("ok", 200, rProduct), null, 200);
     } 
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<JsonResponse> destroy (@PathVariable long id) {
+        try {
+            productRepository.deleteById(id);
+        } catch (Exception e) {
+            return new ResponseEntity<JsonResponse>(new JsonResponse(e.getMessage(), 500, null), null, 500);
+        }
+        return new ResponseEntity<JsonResponse>(new JsonResponse("Product deleted successfully", 200, null), null, 200);
+    }
     
 }
