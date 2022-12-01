c7222665aa065c0320faa5c81b9115ed2fdf10a7
Gabriel Ramos <gabriel.olv2002@gmail.com>
2022-12-01 18:35:02 +0000

    remove unused Exceptions folder

--- src/main/java/com/gbrramos/lix/exceptions/Throwable.java
+++
@@ -1,9 +0,0 @@
-package com.gbrramos.lix.exceptions;
-
-public class Throwable {
-    public static final String NOT_FOUND_STRING = "Not found";
-
-    private Throwable() {
-        super();
-    }
-}
